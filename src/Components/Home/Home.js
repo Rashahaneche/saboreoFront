69dc8dd51d257cd58ceec8f3b29cb8a362802aba
Rasha Haneche <rashahaneche@MBP-de-Rasha.localdomain>
2021-07-09 20:33:49 +0200

    Hemos creado el carusel

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchBar from '../Searchbar/Searchbar';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -7,41 +7,56 @@ import Cards from '../Cards/Card';
 import axios from 'axios';
 import Macarrones from '../../images/MacarronesConChorizo.jpeg'
 const Home = () => {
+  const [currentSliderOffset, setCurrentSliderOffset] = React.useState(0);
   const [dishes, setDishes] = React.useState([]);
+
+  const move = direction => {
+    console.log('current', currentSliderOffset);
+    if (currentSliderOffset === 0 && direction === 'left') return;
+    if (currentSliderOffset < (dishes.length-4)*(-450) && direction === 'right') return;
+    const offset = 450 * (direction === 'right' ? -1 : 1);
+    console.log('offset', offset);
+    setCurrentSliderOffset(currentSliderOffset + offset)
+  }
   
-  React.useEffect(() => {
-    axios.get('http://localhost:3000/dish/60e2f1b310c0438886eb05a3')
+  useEffect(() => {
+    axios.get('http://localhost:3000/dish')
     .then(function (response) {
-      setDishes([response.data]) 
+      setDishes(response.data) 
     })
-    .catch(function (error) {
+    .catch(error => { 
       console.log(error);
     })
-  },[])
+  },[]);
+
   return (
-    
     <section>
       <Header/>
       <SearchBar/>
       <div className="main-div">
         <div className="title">
-          <h1 > PLATOS POPULARES </h1>
+          <h1>PLATOS POPULARES</h1>
         </div>
-        <div className="card-div">
-          {dishes.length && dishes.map(dish => <Cards 
-            key= {dish._id}
-            title={dish.name}
-            description= {dish.description}
-            image={Macarrones}
-            />)}
+        <div className="carusel-div">
+          <div id="slider" className="slider" style={{transform:`translateX(${currentSliderOffset}px)`}}>  
+              {dishes.length && dishes.map(dish => <Cards 
+                key= {dish._id}
+                title={dish.name}
+                description= {dish.description}
+                image={Macarrones}
+                />)}
+          </div>
+        </div>
+        <div className="Carussel-Nav-Button">
+            <button id="ourLeft" onClick={()=> move("left")}>Move left</button>
+            <button id="outButton" onClick={()=> move("right")}>Move right</button>
         </div>
       </div>
-
       <div>
         <Footer/>
       </div>
     </section>
-  )
+  );
 };
-export default Home;
 
+export default Home;
