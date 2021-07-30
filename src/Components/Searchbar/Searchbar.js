c673bfbdb5610bdcf9a484dada69e2d63276ef51
MiMosqueda <mi.mosqueda@gmail.com>
2021-07-30 18:41:03 +0200

    se añade estado disabled al searchBar

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -6,7 +6,13 @@ const SearchBar = () => {
   const [keyword,setKeyword]= React.useState('')
   const history= useHistory();
   const handleSubmit = () => {
-   history.push(`/search?query=${keyword}`);
+    history.push(`/search?query=${keyword}`);
+  }
+  let disabledButton = true;
+  if (keyword === '') {
+    disabledButton=true
+  } else {
+    disabledButton=false
   }
   return (
     <section className="searchbar-container">
@@ -14,10 +20,14 @@ const SearchBar = () => {
         <input 
         key="random1"
         value={keyword}
-        placeholder={"search for a dish"}
-        onChange={(e) => setKeyword(e.target.value)}
+        placeholder={"  search for a dish"}
+        onChange={(e) => {
+          setKeyword(e.target.value) 
+        }}
         />
-        <button onClick = {handleSubmit}>Buscar</button>
+        <button disabled={disabledButton}
+        className='searchButton' 
+        onClick = {handleSubmit}>  Buscar  </button>
       </div>
     </section>
   );
